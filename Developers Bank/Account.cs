78db6786a828083adc979b61bc7cd271cd580283
Pushon <reshme.pushon2012@gmail.com>
2021-03-08 23:57:51 +0600

    Mid Final Assignment first commit

---
+++ Developers Bank/Account.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developers_Bank
+{
+    class Account
+    {
+        private string accountNumber;
+        private string accountName;
+        private string accountType;
+        private double balance;
+        private string dateOfBirth;
+        private Address address;
+        
+
+        public Account(string accountName,  string dateOfBirth, string accountType, double balance, Address address)
+        {
+            this.accountName = accountName;
+            this.balance = balance;
+            this.address = address;
+            this.accountType = accountType;
+            this.dateOfBirth = dateOfBirth;
+            AccountNumberGen();
+        }
+
+
+
+        public string AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public string AccountType
+        {
+            get { return this.accountType; }
+            set { this.accountType = value; }
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public string DateOfBirth
+        {
+            get { return this.dateOfBirth; }
+            set { this.dateOfBirth = value; }
+        }
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        //This method will auto generate account number. 
+        public void AccountNumberGen()
+         {
+             // 1- is for saving account and 2- is for cheking
+             int start = 999;
+             
+             if(this.accountType == "savings")
+             {
+                start++;
+                this.accountNumber = "1-"  + dateOfBirth + start;
+                
+             }
+             else
+             {
+                start++;
+                this.accountNumber = "2-" + dateOfBirth + start;
+              
+            }
+             
+         }
+
+        
+      /*  public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }*/
+
+
+       
+
+        //Withdraw 
+        public void Withdraw(double amount,string tempType)
+        {
+            
+            if (tempType == "savings" && this.balance - amount >= 500 )
+            {
+               
+                this.balance = this.balance - amount;
+                Console.WriteLine("Withdaw Successful");
+
+            }
+            else if(tempType == "savings" && this.balance - amount < 500)
+            {
+             
+                Console.WriteLine("Withdaw Not possible");
+            }
+            else if (tempType == "checking" && this.balance - amount >= 0)
+            {
+                this.balance = this.balance - amount;
+                Console.WriteLine("Withdaw Successful");
+            }
+            else
+            {
+                
+              Console.WriteLine("Withdaw Not possible");
+                
+
+            }
+
+           
+
+        }
+
+        //Tranfer
+        public void Transfer(ref string receiver, double amount)
+        {
+            Console.WriteLine("Transfer working");
+            Console.WriteLine(receiver);
+            Console.WriteLine(amount);
+
+            this.balance = this.balance + amount;
+            Console.WriteLine("Transfer Successfully Completed");
+
+
+        }
+
+        //Deposite
+        public void Deposite(double amount)
+        {
+
+            this.balance = this.balance + amount;
+            Console.WriteLine("Deposited!!");
+        }
+
+        public void PrintAccount()
+        {
+
+            Console.WriteLine("\nAccount No:{0}\nAccount Name:{1}\nBalance:{2}\nDate of Birth: {3} ", this.accountNumber, this.accountName, this.balance, this.dateOfBirth);
+            this.address.PrintAddress();
+        }
+
+    }
+}
---
+++ Developers Bank/Bank.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developers_Bank
+{
+    class Bank
+    {
+        private string bankName;
+        private static Account[] myBank;
+        public static int size;
+        public int flag = 0;
+        public string chooice;
+        private string teampAccount;
+        private double teampAmount;
+        private string receiver;
+      
+       
+        public Bank(string bankName,int size)
+        {
+            Bank.size = size;
+            this.bankName = bankName;
+            myBank = new Account[size];
+        }
+        public void AccessAccount()
+        {
+            while (flag == 0)
+            {
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine(" \t  Make a  Transactions  With Developers Bank LTD");
+                Console.WriteLine("----------------------------------------------------------------");
+
+                Console.WriteLine("Please Enter a Command to Progress: \n");
+                Console.WriteLine("deposit --> Make a deposit\nwithdraw --> Make a withdrawal\ntransfer --> Make a transfer\nshow --> Show the number transactions and balance\nquit --> Exit the application ");
+                chooice = Console.ReadLine();
+
+                switch (chooice)
+                {
+                    case "deposit":
+
+                        Console.WriteLine("Please Enter a Account Number: \n");
+                        teampAccount = Console.ReadLine();
+
+                        Console.WriteLine("Please Enter Diposit Amount: \n");
+                        teampAmount = Convert.ToDouble(Console.ReadLine());
+
+                        Transaction(teampAmount, chooice, teampAccount);
+                       
+
+                        break;
+
+                    case "withdraw":
+                        
+                        Console.WriteLine("Please Enter a Account Number: \n");
+                        teampAccount = Console.ReadLine();
+
+                        Console.WriteLine("Please Enter Withdraw Amount: \n");
+                        teampAmount = Convert.ToDouble(Console.ReadLine());
+
+                        Transaction(teampAmount, chooice ,teampAccount);
+
+                        break;
+
+                    case "transfer":
+                        Console.WriteLine("Please Enter a Receiver Account Number: \n");
+                        receiver = Console.ReadLine();
+
+                        Console.WriteLine("Please Enter Transfer Amount: \n");
+                        teampAmount = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Please Your Account Number: \n");
+                        teampAccount = Console.ReadLine();
+
+                        Transaction(teampAmount, chooice, teampAccount, receiver);
+
+
+                        break;
+                    case "show":
+
+
+
+                        break;
+                    case "quit":
+
+                        flag = 1;
+
+                        break;
+                    default:
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("Wrong Input!\nPlease try Again.");
+                        Console.WriteLine("--------------------------------");
+                        break;
+                }
+
+            }
+        }
+
+        
+
+        public Account[] MyBank
+        {
+            get
+            {
+                return myBank;
+            }
+            set
+            {
+                myBank = value;
+            }
+        }
+        public Account[] Accounts
+        {
+            set { Bank.myBank = value; }
+            get { return Bank.myBank; }
+        }
+
+        public int Flag
+        {
+            set { this.flag = value; }
+            get { return this.flag; }
+        }
+
+
+        public void Transaction( double teampAmmount, string transType, params string[] optionalParams)
+        {
+            string tempAcn;
+            string tempAcn2;
+
+           try
+            {
+                tempAcn = optionalParams[0];
+                tempAcn2 = optionalParams[1];
+               
+            }
+            catch (Exception e)
+            {
+                tempAcn = optionalParams[0];
+                tempAcn2 = optionalParams[0];
+            }
+
+            for (int i = 0; i < myBank.Length; i++)
+            {
+               
+                if (myBank[i] == null)
+                {
+                    continue;
+                    Console.WriteLine("Transaction method null case");
+                }
+                else if ( myBank[i].AccountNumber == tempAcn && transType == "deposite")
+                {
+                    myBank[i].Deposite(teampAmmount);
+                    Console.WriteLine("Transaction method dipo");
+                    break;
+                }
+                else if (myBank[i].AccountNumber == tempAcn && transType == "withdraw")
+                {
+                    
+                    Console.WriteLine("withdraw working");
+                    myBank[i].Withdraw(teampAmmount, myBank[i].AccountType);
+                   // check = true;
+                    break;
+                }
+                else if (myBank[i].AccountNumber == tempAcn && transType == "transfer")
+                {
+                    Console.WriteLine("trans  working");
+                   
+                    myBank[i].Withdraw(teampAmmount, myBank[i].AccountType);
+                    for (int j = 0; j < myBank.Length; j++)
+                    {
+                        if (myBank[j].AccountNumber == tempAcn2)
+                        {
+                            
+                            myBank[j].Transfer(ref tempAcn2, teampAmmount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Receiver account not found"); //
+                        }
+                    }
+                       
+
+                   
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Transaction method no case");
+                }
+
+            }
+
+        }
+
+      
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    break;
+                }
+            }
+        }
+
+        public void Savings()
+        {
+
+        }
+
+        public void Checking()
+        {
+
+        }
+        public void PrintAllAccounts()
+        {
+            Console.WriteLine("-----------------" + bankName + "-----------------");
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    continue;
+                }
+                myBank[i].PrintAccount();
+            }
+            Console.WriteLine("-----------------" + "######" + "-----------------");
+        }
+    }
+}
